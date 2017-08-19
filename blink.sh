b91013d6572a37fba96cc4532ef9c4a2b2bcc019
Krzysztof Wolny <k.wolny@smartrecruiters.com>
2017-08-19 20:17:21 +0200

    refactored

--- blink.sh
+++ blink.sh
@@ -3,21 +3,16 @@
 LED=$1
 LED_TIME=0.5
 
-function colors {
-  gpio -g write $LED $1
+function write {
+  gpio -g write $1 $2
 }
 
 echo "Blinking GPIO=$1"
 
 gpio -g mode $LED output
 
-colors 0
-
 for ((c=0; c<2; c=1-c))
 do
-  # colors 1
-  # sleep $LED_TIME
-
-  colors $c
+  write $LED $c
   sleep $LED_TIME
 done
