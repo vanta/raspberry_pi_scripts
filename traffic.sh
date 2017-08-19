1a67cc3d9b4bc454b7bd9a89d97cc07d69c51bbf
Krzysztof Wolny <k.wolny@smartrecruiters.com>
2017-08-19 21:48:36 +0200

    Refactor

--- traffic.sh
+++ traffic.sh
@@ -4,9 +4,13 @@ GREEN=24
 RED=18
 YELLOW=17
 
-GREEN_TIME=10
+GREEN_TIME=5
 YELLOW_TIME=1
-RED_TIME=10
+RED_TIME=5
+
+function output(no) {
+  gpio -g mode $no output
+}
 
 function colors {
   gpio -g write $RED $1
@@ -14,15 +18,18 @@ function colors {
   gpio -g write $GREEN $3
 }
 
-echo "Start"
+echo "Start Traffic"
 
-gpio -g mode $RED output
-gpio -g mode $YELLOW output
-gpio -g mode $GREEN output
+# gpio -g mode $RED output
+# gpio -g mode $YELLOW output
+# gpio -g mode $GREEN output
+output($RED)
+output($YELLOW)
+output($GREEN)
 
 colors 0 0 0
 
-for ((c=0; c<2; c=1-c ))
+for ((;;))
 do
   echo "RED for $RED_TIME s"
   colors 1 0 0
@@ -39,9 +46,7 @@ do
   echo "YELLOW for $YELLOW_TIME s"
   colors 0 1 0
   sleep $YELLOW_TIME
-  
+
 done
 
 echo "Stop"
-
-
