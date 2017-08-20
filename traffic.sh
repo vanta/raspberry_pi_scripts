2eec75cbf6e52a81d9463646b58f2f9bdd2195cb
Krzysztof Wolny <k.wolny@smartrecruiters.com>
2017-08-20 12:22:28 +0200

    2nd lights

--- traffic.sh
+++ traffic.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # RED YELLOW GREEN
-LIGHTS=(10 11 9)
-LIGHTS_TIMES=(5 1 5)
+LIGHTS1=(10 11 9)
+LIGHTS2=(25 7 8)
 
 GREEN_TIME=5
 YELLOW_TIME=1
@@ -12,38 +12,52 @@ function output {
   gpio -g mode $1 output
 }
 
-function colors {
-  gpio -g write ${LIGHTS[0]} $1
-  gpio -g write ${LIGHTS[1]} $2
-  gpio -g write ${LIGHTS[2]} $3
+function colors1 {
+  gpio -g write ${LIGHTS1[0]} $1
+  gpio -g write ${LIGHTS1[1]} $2
+  gpio -g write ${LIGHTS1[2]} $3
+}
+
+function colors2 {
+  gpio -g write ${LIGHTS2[0]} $1
+  gpio -g write ${LIGHTS2[1]} $2
+  gpio -g write ${LIGHTS2[2]} $3
 }
 
 echo "Start Traffic"
 
-output ${LIGHTS[0]}
-output ${LIGHTS[1]}
-output ${LIGHTS[2]}
+output ${LIGHTS1[0]}
+output ${LIGHTS1[1]}
+output ${LIGHTS1[2]}
 
-colors 0 0 0
+output ${LIGHTS2[0]}
+output ${LIGHTS2[1]}
+output ${LIGHTS2[2]}
+
+colors1 0 0 0
+colors2 0 0 0
 
 for ((;;))
 do
-  echo "RED for $RED_TIME s"
-  colors 1 0 0
+  # echo "RED for $RED_TIME s"
+  colors1 1 0 0
+  colors2 0 0 1
   sleep $RED_TIME
 
-  echo "RED + YELLOW for $YELLOW_TIME s"
-  colors 1 1 0
+  # echo "RED + YELLOW for $YELLOW_TIME s"
+  colors1 1 1 0
+  colors2 0 1 0
   sleep $YELLOW_TIME
 
-  echo "GREEN for $GREEN_TIME s"
-  colors 0 0 1
+  # echo "GREEN for $GREEN_TIME s"
+  colors1 0 0 1
+  colors2 1 0 0
   sleep $GREEN_TIME
 
-  echo "YELLOW for $YELLOW_TIME s"
-  colors 0 1 0
+  # echo "YELLOW for $YELLOW_TIME s"
+  colors1 0 1 0
+  colors2 1 1 0
   sleep $YELLOW_TIME
-
 done
 
 echo "Stop"
